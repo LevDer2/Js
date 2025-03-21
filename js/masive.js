99de91b1e23782a041ca761b17b90193066561c2
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-21 21:01:34 +0200

    massive

---
+++ js/masive.js
@@ -0,0 +1,34 @@
+// const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque saepe eos deleniti nesciunt dolore culpa libero, nostrum nihil temporibus sed blanditiis quam, ea maiores sapiente est, nam odio voluptatem officiis."
+// const result = text.split("");
+// console.log(result);
+
+// const massive = ["Lev", "Vlad", "Vlad-Draga", "Nazar", "Mukola", "Rostuslav"];
+// const result = massive.join(" , ");
+// console.log(result);
+
+// const massive = ["Lev", "Vlad", "Vlad-Draga", "Nazar", "Mukola", "Rostuslav", "Lev"];
+// // const result = massive.indexOf("Lev");
+// const result = massive.includes("lev");
+// console.log(result);
+
+// const message = "Welcome to Ukraine!" Розбити цю строку по буквах в масив, та вивести цей масив у консоль,
+// також вивести у консоль індекс літери "l" маленька літера за допомогою вбудованого метода. 
+// Після цього елементи масива поєднати в строку і вивести строку в консоль
+// const message = "Welcome to Ukraine!";
+// const res = message.split("");
+// const resu = message.indexOf("l");
+// const result = message.split("").join(" ");
+// console.log(res);
+// console.log(resu);
+// console.log(result);
+
+// створити масив гравців, додати до кожного гравця N, де N це порядковий номер.
+// Наприклад "Polly" => "Polly-1"
+
+// const massive = ["Lev", "Vlad", "Vlad-Draga", "Nazar", "Mukola", "Rostuslav"];
+// let counter = 0;
+// for (let user of massive) {
+//     counter += 1;
+//     const something = (`${user}-${counter}`)
+//     console.log(something);
+// }
